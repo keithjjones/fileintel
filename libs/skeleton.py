0bba157aff174d88955c9ae163abfd61d7d1fcbe
Keith J. Jones <keith@keithjjones.com>
2016-09-17 15:58:21 -0400

    Cleaned up formatting.

--- libs/skeleton.py
+++ libs/skeleton.py
@@ -10,21 +10,22 @@
 # CLASSES
 #
 
+
 class Skeleton(object):
     """
     Class to hold Skeleton items.
     """
     def __init__(self):
         pass
-    
-    """
-    Adds appropriate headers to input list.
-    """
-    def add_headers(self,inputheaders):
+
+    def add_headers(self, inputheaders):
+        """
+        Adds appropriate headers to input list.
+        """
         pass
 
-    """
-    Adds the pulled data to the input row.
-    """    
-    def add_row(self,filehash,inputrow):
+    def add_row(self, filehash, inputrow):
+        """
+        Adds the pulled data to the input row.
+        """
         pass
